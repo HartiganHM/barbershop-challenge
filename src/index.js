500bb287980f940bb45eebf1f04a32ccc9bb35ba
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-08 12:03:08 -0700

    Build out router and store in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import App from './App/App';
+import './normalize.css';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(
+  rootReducer,
+  devTools,
+  applyMiddleware(thunk, logger)
+);
+
+const router = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
+ReactDOM.render(router, document.getElementById('root'));
