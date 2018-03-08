9509b8e311e99f579e52318c0606508dd2175afb
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-08 16:10:57 -0700

    Build initial Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import iconData from '../../data/iconData';
 import './Header.css';
 
 const Header = () => {
   return (
     <div className="Header">
-      <h1>Soi una Header</h1>
+      <h1>My Photos</h1>
+
+      <span className="button-wrapper">
+        <a className="view-button">
+          <svg className="header-icon" width="13" height="10" viewBox="0 0 13 10">
+            <path d={iconData.gridIcon} />
+          </svg>
+          Grid
+        </a>
+
+        <a className="view-button">
+          <svg className="header-icon" width="13" height="10" viewBox="0 0 13 10">
+            <path d={iconData.listIcon} />
+          </svg>
+          List
+        </a>
+      </span>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
