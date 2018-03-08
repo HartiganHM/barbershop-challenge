c6b2624923a0e62de3da639de6a0500b32b1229d
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-08 13:49:31 -0700

    Import components into Routes

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
+import List from '../List/List';
+import Grid from '../Grid/Grid';
+import Details from '../Details/Details';
+import StyleGuide from '../StyleGuide/StyleGuide';
 import * as action from '../../actions';
 
 class Routes extends Component {
