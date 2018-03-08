c9de09b087634c67ad816535631dfca59bde3416
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-08 15:16:46 -0700

    Switch getPhotos to fetch popular photos

--- src/helpers/getPhotos/getPhotos.js
+++ src/helpers/getPhotos/getPhotos.js
@@ -2,7 +2,7 @@ import unsplash from '../unsplash/unsplash';
 
 const getPhotos = async () => {
   try {
-    const fetchedPhotos = await unsplash.photos.listPhotos(1, 6, "latest");
+    const fetchedPhotos = await unsplash.photos.listPhotos(1, 6, "popular");
     const jsonPhotos = await fetchedPhotos.json();
     console.log(jsonPhotos)
     return jsonPhotos
