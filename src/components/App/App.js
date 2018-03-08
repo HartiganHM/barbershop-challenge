6f041a02a18e8e626f543c03b6d1b958f0483af2
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-08 12:51:32 -0700

    Route to Routes component on root endpoint from App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Routes from '../Routes/Routes';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>I am an APP!</h1>
+        <Route to="/" component={Routes} />
       </div>
     );
   }
