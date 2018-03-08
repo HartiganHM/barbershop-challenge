696bd70948098d3d787aff71c5ff23004fbca633
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-08 11:51:49 -0700

    Pass initial App component test to check testing environment

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App tests', () => {
+  it('Should match the snapshot', () => {
+    const renderedApp = shallow(<App />);
+
+    expect(renderedApp).toMatchSnapshot();
+  });
 });
