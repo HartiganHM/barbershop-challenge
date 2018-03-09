51dcaad21ddd35590f3ece4c057eecc1d78d2c12
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-09 11:54:23 -0700

    Remove console.log from getPhotos

--- src/helpers/getPhotos/getPhotos.js
+++ src/helpers/getPhotos/getPhotos.js
@@ -4,7 +4,7 @@ const getPhotos = async () => {
   try {
     const fetchedPhotos = await unsplash.photos.listPhotos(1, 6, "popular");
     const jsonPhotos = await fetchedPhotos.json();
-    console.log(jsonPhotos)
+
     return jsonPhotos
   } catch (error) {
     throw Error(`Error retrieving photos from Unsplash: ${error}`)
