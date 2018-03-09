1810beba0708d2789105c0421770dff4e369ca54
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-09 15:22:58 -0700

    Setup Details component skeleton structure

---
+++ src/components/Details/Details.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import './Details.css';
+
+const Details = () => {
+  return (
+    <div className="Details">
+      <h1>Yo! ME DUH DETAILS</h1>
+    </div>
+  );
+};
+
+export default Details;
