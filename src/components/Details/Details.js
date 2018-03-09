9fd3e44888a0dc3ebae7c162005236b88f44c967
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-09 15:49:54 -0700

    Render needed photo information in Details component

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,18 +1,35 @@
+/*eslint-disable camelcase*/
+
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import './Details.css';
 
-const Details = () => {
+const Details = props => {
+  const { selectedPhoto } = props;
+  const imageSource = selectedPhoto.urls.small;
+  const imageAlt = selectedPhoto.description;
+  const userFullName = selectedPhoto.user.name;
+  const userImage = selectedPhoto.user.profile_image.large;
+  const userUsername = selectedPhoto.user.username;
+  const userLocation = selectedPhoto.user.location;
+
   return (
     <div className="Details">
-      <h1>Yo! ME DUH DETAILS</h1>
+      <img src={imageSource} alt={imageAlt} />
+      <h3>{userFullName}</h3>
+      <img src={userImage} alt={userUsername} />
+      {userLocation && (
+        <span className="location-wrapper">
+          <h4 className="location">{userLocation}</h4>
+        </span>
+      )}
     </div>
   );
 };
 
 const mapStateToProps = store => ({
-  selectedPhoto: store.selectedPhoto;
+  selectedPhoto: store.selectedPhoto
 });
 
 export default connect(mapStateToProps, null)(Details);
@@ -20,3 +37,8 @@ export default connect(mapStateToProps, null)(Details);
 Details.propTypes = {
   selectedPhoto: PropTypes.object
 };
+
+//photo
+//user image
+//username
+//location
