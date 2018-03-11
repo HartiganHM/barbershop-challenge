b33bf80277b0f60cd01bbd41a2f1b7a0bbc0c0ab
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-11 10:43:16 -0600

    Refactor getPhotos helper to only pull 6 photos at a time

--- src/helpers/getPhotos/getPhotos.js
+++ src/helpers/getPhotos/getPhotos.js
@@ -4,9 +4,8 @@ const getPhotos = async currentPhotoLength => {
   try {
     const photoLength = currentPhotoLength ? currentPhotoLength : 0;
     const photoStart = photoLength + 1;
-    const photoEnd = photoLength + 6;
 
-    const fetchedPhotos = await unsplash.photos.listPhotos(photoStart, photoEnd, 'popular');
+    const fetchedPhotos = await unsplash.photos.listPhotos(photoStart, 6, 'popular');
     const jsonPhotos = await fetchedPhotos.json();
 
     return jsonPhotos;
