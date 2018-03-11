86645d3fc32b17a54758eb1b73f4a6c185e3dd1a
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-10 17:53:31 -0700

    Pass currentPhotoLength to populatePhotos action

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,11 @@ import getPhotos from '../helpers/getPhotos/getPhotos';
 import saveInitialPhotos from '../helpers/saveInitialPhotos/saveInitialPhotos';
 import getSavedPhotos from '../helpers/getSavedPhotos/getSavedPhotos';
 
-export const populatePhotos = () => async dispatch => {
+export const populatePhotos = currentPhotoLength => async dispatch => {
   let photos = getSavedPhotos();
 
   if (!photos) {
-    photos = await getPhotos();
+    photos = await getPhotos(currentPhotoLength);
   }
 
   saveInitialPhotos(photos);
