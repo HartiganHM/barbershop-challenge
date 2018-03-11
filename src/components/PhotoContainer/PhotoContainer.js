759ad58d59797ff3f5f1f5e01752459a5d203c86
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-11 11:25:20 -0600

    Remove instances of GetPhotosButton

--- src/components/PhotoContainer/PhotoContainer.js
+++ src/components/PhotoContainer/PhotoContainer.js
@@ -1,35 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Photo from '../Photo/Photo';
-import iconData from '../../data/iconData';
-import * as actions from '../../actions';
 import PropTypes from 'prop-types';
 import './PhotoContainer.css';
 
 const PhotoContainer = props => {
-  const { photos, currentView, getMorePhotos } = props;
+  const { photos, currentView } = props;
   const currentClass = 'PhotoContainer ' + currentView;
 
   const renderedPhotos = photos.map(photo => {
     return <Photo key={photo.id} photoData={photo} currentView={currentView} />;
   });
 
-  return (
-    <div className={currentClass}>
-      {renderedPhotos}
-
-      <a
-        className="get-photos"
-        onClick={() => getMorePhotos(photos.length)}
-      >
-        <svg viewBox="0 0 15 14" className="get-photos-icon">
-          <path d={iconData.getPhotos} />
-        </svg>
-
-        <span className="get-photos-text">Get Photos</span>
-      </a>
-    </div>
-  );
+  return <div className={currentClass}>{renderedPhotos}</div>;
 };
 
 const mapStateToProps = store => ({
@@ -37,12 +20,7 @@ const mapStateToProps = store => ({
   currentView: store.currentView
 });
 
-const mapDispatchToProps = dispatch => ({
-  getMorePhotos: currentPhotoLength =>
-    dispatch(actions.getMorePhotos(currentPhotoLength))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(PhotoContainer);
+export default connect(mapStateToProps, null)(PhotoContainer);
 
 PhotoContainer.propTypes = {
   photos: PropTypes.array,
