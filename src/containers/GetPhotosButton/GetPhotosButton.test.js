9c66dec937a7c36845e0e79c6e2955f2b5e70e6d
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-12 07:38:12 -0600

    Finish MDP tests for GetPhotosButton

--- src/containers/GetPhotosButton/GetPhotosButton.test.js
+++ src/containers/GetPhotosButton/GetPhotosButton.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { GetPhotosButton, mapStateToProps, mapDispatchToProps } from './GetPhotosButton';
+import {
+  GetPhotosButton,
+  mapStateToProps,
+  mapDispatchToProps
+} from './GetPhotosButton';
 import mockPhoto from '../../data/mockData/mockPhoto';
 import * as actions from '../../actions';
 
-describe('GetPhotosButton tests', () => {
-
-});
+describe('GetPhotosButton tests', () => {});
 
 describe('mapStateToProps tests', () => {
   it('Should pull photos from store', () => {
@@ -19,5 +21,11 @@ describe('mapStateToProps tests', () => {
 });
 
 describe('mapDispatchToProps tests', () => {
+  it('Should call dispatch when getMorePhotos is called', () => {
+    const mockDispatch = jest.fn();
+    const result = mapDispatchToProps(mockDispatch);
 
+    result.getMorePhotos();
+    expect(mockDispatch).toHaveBeenCalled();
+  });
 });
