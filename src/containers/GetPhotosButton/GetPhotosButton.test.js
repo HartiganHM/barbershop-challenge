b40ff77fecb19b2547180d3f9853fcc495515bdf
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-12 07:54:34 -0600

    Mock out anticipated tests for GetPhotosButton

--- src/containers/GetPhotosButton/GetPhotosButton.test.js
+++ src/containers/GetPhotosButton/GetPhotosButton.test.js
@@ -8,7 +8,40 @@ import {
 import mockPhoto from '../../data/mockData/mockPhoto';
 import * as actions from '../../actions';
 
-describe('GetPhotosButton tests', () => {});
+describe('GetPhotosButton tests', () => {
+  let photos;
+  let getMorePhotos;
+  let renderedGetPhotosButton;
+
+  beforeEach(() => {
+    photos = [mockPhoto];
+    getMorePhotos = jest.fn();
+
+    renderedGetPhotosButton = shallow(
+      <GetPhotosButton photos={photos} getMorePhotos={getMorePhotos} />
+    );
+  });
+
+  it('Should match the snapshot', () => {
+    
+  });
+
+  it('Should match imageTotal to number of photos in store', () => {
+
+  });
+
+  it('Should update imageTotal with handleClick and call getMorePhotos', () => {
+
+  });
+
+  it('Should have a class of loading and a loader if imageTotal does not match photos total', () => {
+
+  });
+
+  it('Should not have a class of loading or a loader if imageTotal matches photos total', () => {
+
+  });
+});
 
 describe('mapStateToProps tests', () => {
   it('Should pull photos from store', () => {
