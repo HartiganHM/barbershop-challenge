d689058eface841cc31cb9e76c1b2b6d5ad2e901
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-11 20:17:52 -0600

    Add loader to GetPhotosButton component and change to stateful component

--- src/components/GetPhotosButton/GetPhotosButton.js
+++ src/components/GetPhotosButton/GetPhotosButton.js
@@ -1,35 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import iconData from '../../data/iconData';
 import * as actions from '../../actions';
 import PropTypes from 'prop-types';
 import './GetPhotosButton.css';
 
-const GetPhotosButton = props => {
-  const { photos, getMorePhotos } = props;
-  const plusIcon = (
-    <svg viewBox="0 0 13.33 13.33" className="plus-icon">
-      <path d={iconData.getPhotos} />
-    </svg>
-  );
+class GetPhotosButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageTotal: 0
+    };
+  }
 
-  const loader = (
-    <div className="loader">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  );
+  componentDidMount() {
+    const imageTotal = this.props.photos.length;
 
-  return (
-    <a className="GetPhotosButton" onClick={() => getMorePhotos(photos.length)}>
-      {plusIcon}
+    this.setState({ imageTotal });
+  }
 
+  handleClick(photos) {
+    const { getMorePhotos } = this.props;
+    const imageTotal = this.state.imageTotal + 6;
+
+    this.setState({ imageTotal });
+    getMorePhotos(photos.length);
+  }
+
+  render(props) {
+    const { imageTotal } = this.state;
+    const { photos } = this.props;
+    const currentClass =
+      imageTotal === photos.length
+        ? 'GetPhotosButton'
+        : 'GetPhotosButton loading';
+
+    const enabledButton = [
+      <svg viewBox="0 0 13.33 13.33" className="plus-icon">
+        <path d={iconData.getPhotos} />
+      </svg>,
       <span className="get-photos-text">Get Photos</span>
-    </a>
-  );
-};
+    ];
+
+    const loader = (
+      <div className="loader">
+        <div />
+        <div />
+        <div />
+        <div />
+      </div>
+    );
+
+    return (
+      <a className={currentClass} onClick={() => this.handleClick(photos)}>
+        {imageTotal === photos.length ? enabledButton : loader}
+      </a>
+    );
+  }
+}
 
 const mapStateToProps = store => ({
   photos: store.photos
