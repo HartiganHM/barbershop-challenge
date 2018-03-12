958e006b825d44c33fcfcb46e26ac47a2fc9c5ab
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-12 09:36:42 -0600

    Complete Photo test suite

--- src/containers/Photo/Photo.test.js
+++ src/containers/Photo/Photo.test.js
@@ -25,23 +25,49 @@ describe('Photo tests', () => {
   });
 
   it('Should match the snapshot', () => {
-
+    expect(renderedPhoto).toMatchSnapshot();
   });
 
   it('Should show photo-details-link if currentView is List', () => {
+    const expected = 1;
 
+    expect(renderedPhoto.find('.photo-details-link').length).toEqual(expected);
   });
 
   it('Should not show photo-details-link if currentView is Grid', () => {
+    const expected = 0;
 
+    currentView = 'Grid';
+    renderedPhoto = shallow(
+      <Photo
+        photoData={photoData}
+        currentView={currentView}
+        sendSelectedPhotoToStore={sendSelectedPhotoToStore}
+      />
+    );
+
+    expect(renderedPhoto.find('.photo-details-link').length).toEqual(expected);
   });
 
   it('Should have a class of list-image if currentView is List', () => {
+    const expected = 1;
 
+    expect(renderedPhoto.find('.list-image').length).toEqual(expected);
   });
 
   it('Should have a class of grid-image if currentView is Grid', () => {
+    const expected = 1;
 
+    currentView = 'Grid';
+    renderedPhoto = shallow(
+      <Photo
+        photoData={photoData}
+        currentView={currentView}
+        sendSelectedPhotoToStore={sendSelectedPhotoToStore}
+      />
+    );
+
+    expect(renderedPhoto.find('.grid-image').length).toEqual(expected);
   });
 });
 
