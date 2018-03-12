76b6ffc39edfc6480acf7fa130d6e5c2a0d6462b
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-12 09:32:21 -0600

    Mock out anticipated tests for Photo container

--- src/containers/Photo/Photo.test.js
+++ src/containers/Photo/Photo.test.js
@@ -5,7 +5,44 @@ import mockPhoto from '../../data/mockData/mockPhoto';
 import * as actions from '../../actions';
 
 describe('Photo tests', () => {
+  let photoData;
+  let currentView;
+  let sendSelectedPhotoToStore;
+  let renderedPhoto;
 
+  beforeEach(() => {
+    photoData = mockPhoto;
+    currentView = 'List';
+    sendSelectedPhotoToStore = jest.fn();
+
+    renderedPhoto = shallow(
+      <Photo
+        photoData={photoData}
+        currentView={currentView}
+        sendSelectedPhotoToStore={sendSelectedPhotoToStore}
+      />
+    );
+  });
+
+  it('Should match the snapshot', () => {
+
+  });
+
+  it('Should show photo-details-link if currentView is List', () => {
+
+  });
+
+  it('Should not show photo-details-link if currentView is Grid', () => {
+
+  });
+
+  it('Should have a class of list-image if currentView is List', () => {
+
+  });
+
+  it('Should have a class of grid-image if currentView is Grid', () => {
+
+  });
 });
 
 describe('mapDispatchToProps tests', () => {
