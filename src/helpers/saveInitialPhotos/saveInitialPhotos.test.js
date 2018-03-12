c6d222faeaae7aca71e3057b03934b0abc5309d4
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-12 11:01:27 -0600

    Complete saveInitialPhotos tests

---
+++ src/helpers/saveInitialPhotos/saveInitialPhotos.test.js
@@ -0,0 +1,21 @@
+import saveInitialPhoto from './saveInitialPhotos';
+import mockPhoto from '../../data/mockData/mockPhoto';
+
+global.localStorage = {
+  setItem: (key, value) => {
+    return (localStorage[key] = value);
+  }
+};
+
+describe('getSavedPhotos tests', () => {
+  it('Should be a function', () => {
+    expect(saveInitialPhoto).toBeAFuntion;
+  });
+
+  it('Should set photos to localStorage', () => {
+    const mockReturn = saveInitialPhoto([mockPhoto]);
+    const localStorageKey = 'hh-barbershop3';
+
+    expect(localStorage[localStorageKey]).toEqual(JSON.stringify([mockPhoto]));
+  });
+});
