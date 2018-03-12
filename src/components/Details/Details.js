c46acc3727bf78c2aa0dad3785d753901ea753c8
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-03-11 19:16:52 -0600

    Convert Details component to stateful component for loading image

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,95 +1,113 @@
 /*eslint-disable camelcase*/
 
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import iconData from '../../data/iconData';
 import PropTypes from 'prop-types';
 import './Details.css';
 
-const Details = props => {
-  window.scrollTo(0, 0);
+class Details extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false
+    };
+  }
 
-  const { selectedPhoto } = props;
-  const { goBack } = props.history;
+  handleLoad() {
+    const loaded = true;
+    this.setState({ loaded });
+  }
 
-  const fullImage = selectedPhoto.urls.full;
-  const regularImage = selectedPhoto.urls.regular;
+  render(props) {
+    window.scrollTo(0, 0);
 
-  const imageAlt = selectedPhoto.description;
-  const userFullName = selectedPhoto.user.name;
-  const userImage = selectedPhoto.user.profile_image.large;
-  const userUsername = '@' + selectedPhoto.user.username;
-  const userProfile = selectedPhoto.user.links.html;
-  const userLocation = selectedPhoto.user.location;
+    const { selectedPhoto } = this.props;
+    const { goBack } = this.props.history;
+    const { loaded } = this.state;
 
-  const loader = (
-    <div className="loader">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  );
+    const fullImage = selectedPhoto.urls.full;
+    const regularImage = selectedPhoto.urls.regular;
 
-  return (
-    <div className="Details">
-      <svg
-        className="close-icon"
-        onClick={() => goBack()}
-        viewBox="0 0 17.99 17.99"
-      >
-        <rect
-          x="8.5"
-          y="-3.22"
-          width="1"
-          height="24.44"
-          transform="translate(-3.73 8.99) rotate(-45)"
-        />
-        <rect
-          x="8.5"
-          y="-3.21"
-          width="1"
-          height="24.43"
-          transform="translate(8.99 -3.73) rotate(45)"
-        />
-      </svg>
+    const imageAlt = selectedPhoto.description;
+    const userFullName = selectedPhoto.user.name;
+    const userImage = selectedPhoto.user.profile_image.large;
+    const userUsername = '@' + selectedPhoto.user.username;
+    const userProfile = selectedPhoto.user.links.html;
+    const userLocation = selectedPhoto.user.location;
 
-      <div className="wrapper">
-        {
-          !fullImage ? (loader) : (
-            <picture>
-              <source srcSet={regularImage} media="(max-width: 1260px)" />
-              <img className="selected-image" src={fullImage} alt={imageAlt} />
-            </picture>
-          )
-        }
-
-        <a className="user-wrapper" target="_blank" href={userProfile}>
-          <img className="user-image" src={userImage} alt={userUsername} />
-
-          <h2 className="fullname">{userFullName}</h2>
-
-          <h3 className="username">{userUsername}</h3>
-        </a>
+    const loader = (
+      <div className="loader">
+        <div />
+        <div />
+        <div />
+        <div />
       </div>
+    );
 
-      <span className="location-wrapper">
+    return (
+      <div className="Details">
         <svg
-          className="location-icon"
-          width="15"
-          height="20"
-          viewBox="0 0 15 20"
+          className="close-icon"
+          onClick={() => goBack()}
+          viewBox="0 0 17.99 17.99"
         >
-          <path d={iconData.location} />
+          <rect
+            x="8.5"
+            y="-3.22"
+            width="1"
+            height="24.44"
+            transform="translate(-3.73 8.99) rotate(-45)"
+          />
+          <rect
+            x="8.5"
+            y="-3.21"
+            width="1"
+            height="24.43"
+            transform="translate(8.99 -3.73) rotate(45)"
+          />
         </svg>
 
-        <h4 className="location">
-          {userLocation ? userLocation : 'None Listed'}
-        </h4>
-      </span>
-    </div>
-  );
-};
+        <div className="wrapper">
+          {!loaded && loader}
+
+          <picture>
+            <source srcSet={regularImage} media="(max-width: 1260px)" />
+            <img
+              onLoad={() => this.handleLoad()}
+              className="selected-image"
+              src={fullImage}
+              alt={imageAlt}
+            />
+          </picture>
+
+          <a className="user-wrapper" target="_blank" href={userProfile}>
+            <img className="user-image" src={userImage} alt={userUsername} />
+
+            <h2 className="fullname">{userFullName}</h2>
+
+            <h3 className="username">{userUsername}</h3>
+          </a>
+        </div>
+
+        <span className="location-wrapper">
+          <svg
+            className="location-icon"
+            width="15"
+            height="20"
+            viewBox="0 0 15 20"
+          >
+            <path d={iconData.location} />
+          </svg>
+
+          <h4 className="location">
+            {userLocation ? userLocation : 'None Listed'}
+          </h4>
+        </span>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = store => ({
   selectedPhoto: store.selectedPhoto
